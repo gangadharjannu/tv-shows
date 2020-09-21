764c9e854b9a35b762517b39deea2ad9d9969110
gangadharjannu <gangadhar014@gmail.com>
2020-09-21 21:45:29 +0200

    Updates

---
+++ precache-manifest.00381fd7b6bb84c3ae5d28b283768632.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d13d0a294c9309189ef7f45f49f1352",
+    "url": "/tv-shows/index.html"
+  },
+  {
+    "revision": "e552b64adbf899abac5c",
+    "url": "/tv-shows/static/css/main.7d37de78.chunk.css"
+  },
+  {
+    "revision": "9005dfd2ef8731642f31",
+    "url": "/tv-shows/static/js/2.5457d258.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/tv-shows/static/js/2.5457d258.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e552b64adbf899abac5c",
+    "url": "/tv-shows/static/js/main.0856402b.chunk.js"
+  },
+  {
+    "revision": "28b8c78ea779eb2b3067",
+    "url": "/tv-shows/static/js/runtime-main.49286ea2.js"
+  },
+  {
+    "revision": "7f4f7f3cd16e021ff5545a875f7ffbce",
+    "url": "/tv-shows/static/media/RTL_Nederland.7f4f7f3c.svg"
+  }
+]);
